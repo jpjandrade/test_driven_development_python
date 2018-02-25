6cc187499883f8a631136300d6ac7ecfbaa656ba
Joao Pedro Jericó de Andrade <jpjandrade@gmail.com>
2018-02-25 18:19:16 +0100

    First commit with some initial testing in place

---
+++ functional_tests.py
@@ -0,0 +1,46 @@
+from selenium import webdriver
+import unittest
+
+
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+
+        # Edith has heard about a new online to-do app.
+        # She goes check out the homepage
+
+        self.browser.get('http://localhost:8000')
+
+        # She notices the page title and header mentions to-do lists
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+        # She is invited to enter a to-do item straight away
+
+        # She types "Buy peacock feathers" into a text box
+        # (Edith's hobby is tying fly-fishing lures)
+
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item
+
+        # There is still a text box inviting her to add another item.
+        # She ads "use peacock feathers to make a fly"
+
+        # The page updates and shows both items on her list
+
+        # Edith wonders whether the site will remember her list.
+        # She sees that the site has generate a unique URL for her
+
+        # She visists that URL - her to-do list is still there!
+
+        # She goes happily back to sleep
+
+
+if __name__ == '__main__':
+    unittest.main()
